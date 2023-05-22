a7cb0de1650d640d58e7ced5ccaab6da0602deb2
mfallon90 <mfallon7429@sdsu.edu>
2023-05-21 19:40:59 -0700

    added rx fsm

--- primitives/eth_rx_fsm/hdl/eth_rx_fsm.sv
+++ primitives/eth_rx_fsm/hdl/eth_rx_fsm.sv
@@ -1,6 +1,5 @@
 `timescale 1ns / 1ps
-// `define ADDR_VLD ((rx_cnt == 12) & ((mac_addr == P_MAC_ADDR)))
-`define ADDR_VLD ((rx_cnt == 12) & ((mac_addr == P_MAC_ADDR) | (mac_addr == C_BRDCST)))
+`define CTRL_LEN 14
 
 //////////////////////////////////////////////////////////////////////////////////
 //
@@ -30,54 +29,71 @@ module eth_rx_fsm
     input   logic   [7:0]   data_in,
     input   logic           data_in_vld,
     input   logic           byte_in_vld,
-    output  logic    [7:0]  byte_out,
-    output  logic           byte_vld
+    input   logic           crc_vld,
+    output  logic   [7:0]   data_out,
+    output  logic           data_out_vld,
+    output  logic   [123:0] ctrl,
+    output  logic           ctrl_vld
     );
 
-    localparam  [2:0]   S_IDLE  = 0;
-    localparam  [2:0]   S_DEST  = 1;
-    localparam  [2:0]   S_SRC   = 2;
-    localparam  [2:0]   S_TYPE  = 3;
-    localparam  [2:0]   S_PKT   = 4;
+    localparam      S_IDLE  = 0;
+    localparam      S_PKT   = 1;
 
-    logic   [2:0]   state;
+    logic           state;
+    logic   [10:0]  byte_cnt;
+    logic   [103:0] control;
+    logic           frm_done;
 
-  
+    assign frm_done     = crc_vld | ~data_in_vld;
+    assign data_out_vld = (state == S_PKT) & byte_in_vld;
+    assign data_out     = data_in;
+
     always_ff @(posedge clk) begin
         if (~rst_n) begin
-           state    <= S_IDLE;
+            state           <= S_IDLE;
+            byte_cnt        <= 0;
+            control         <= 0;
+            ctrl_vld        <= 0;
+            ctrl            <= 0;
         end
 
         else begin
+            ctrl_vld    <= 0;
             if (byte_in_vld) begin
+
+                control     <= {control[95:0], data_in};
+                byte_cnt    <= byte_cnt+1;
+                ctrl_vld    <= 0;
+
                 case (state)
                     S_IDLE: begin
-                        if (data_in_vld) begin
-                            state       <= S_DEST;
-                            dest_mac    <= {dest_mac[39:0], data_in};
+                        if (~data_in_vld) begin
+                            byte_cnt    <= 0;
+                        end
+                        else if (byte_cnt == `CTRL_LEN-1) begin
+                            state           <= S_PKT;
+                            byte_cnt        <= 0;
+                            ctrl[123:12]    <= {control, data_in};
                         end
                     end
-                    S_DEST: begin
 
-                    end
-                    S_SRC: begin
-
-                    end
-                    S_TYPE: begin
-
-                    end
                     S_PKT: begin
-
+                        if (frm_done) begin
+                            ctrl_vld        <= 1'b1;
+                            ctrl[11:0]      <= { ~crc_vld, byte_cnt};
+                            state           <= S_IDLE;
+                            byte_cnt        <= 0;
+                        end
                     end
                 endcase
             end
         end
     end
 
-    initial begin
-        $dumpfile("eth_rx_fsm.vcd");
-        $dumpvars();
-    end
+    // initial begin
+    //     $dumpfile("eth_rx_fsm.vcd");
+    //     $dumpvars();
+    // end
 
 
 endmodule
--- primitives/eth_rx_fsm/test/test_eth_rx_fsm.py
+++ primitives/eth_rx_fsm/test/test_eth_rx_fsm.py
@@ -7,9 +7,23 @@ sys.path.append("../../../..")
 import test_classes
 from cocotb.clock import Clock
 from cocotb.queue import Queue
-from cocotb.triggers import Timer, RisingEdge, ClockCycles, First, Combine
+from cocotb.triggers import Timer, RisingEdge, ClockCycles, FallingEdge
 
-NUM_WORDS   = 200
+DEST    = b'\x01\x00\x5E\x28\x64\x01'
+SRC     = b'\x2C\xFA\xA2\xA7\x4F\x81'
+TYPE    = b'\x08\x00'
+DATA1   = b'\x46\xC0\x00\x20\xC6\xC9\x00\x00'
+DATA2   = b'\x01\x02\xF4\xE6\x82\xBF\xA0\xFE'
+DATA3   = b'\xE0\xA8\x64\x01\x94\x04\x00\x00'
+DATA4   = b'\x11\x0A\xAA\x4B\xE0\xA8\x64\x01'
+DATA5   = b'\x00\x00\x00\x00\x00\x00\x00\x00'
+DATA6   = b'\x00\x00\x00\x00\x00\x00'
+CRC     = b'\xD0\x1D\x41\x1B'
+
+PACKET = DEST+SRC+TYPE+DATA1+DATA2+DATA3+DATA4+DATA5+DATA6+CRC
+
+print(type(PACKET))
+print(PACKET)
 
 class TB():
     def __init__(self, dut):
@@ -25,28 +39,29 @@ class TB():
         self.log.setLevel(logging.DEBUG)
         self.sb = Queue()
 
-        self.dut.rx_data.value = 0
-        self.dut.rx_dv.value = 0
-        self.dut.rx_er.value = 0
-        self.dut.crs.value = 0
-        self.dut.col.value = 0
+        self.dut.data_in.value = 0
+        self.dut.data_in_vld.value = 0
+        self.dut.byte_in_vld.value = 0
+        self.dut.crc_vld.value = 0
 
-        self.clk = self.dut.rx_clk
+        cocotb.start_soon(Clock(self.dut.clk, 10, units='ns').start())
 
-        self.rmii_driver = test_classes.RmiiDriver(self.clk, self.dut.rx_data, self.dut.rx_dv)
+    async def send_data(self, data_in):
 
-        cocotb.start_soon(Clock(self.clk, 10, units='ns').start())
+        for byte in data_in:
+            self.dut.data_in_vld.value = 1
+            self.dut.data_in.value = byte
+            await FallingEdge(self.dut.byte_in_vld)
 
+            self.dut.data_in_vld.value = 0
+            self.dut.data_in.value = 0
 
-    async def uart_check(self):
-        '''
-        Method to check data against scoreboard
-        '''
-        await RisingEdge(self.dut.uart_word_vld)
-        uart_word = self.dut.uart_word
-        sb_word = await self.sb.get()
-        self.log.info(f'Sent {int(sb_word)}, Received {int(uart_word)}')
-        assert (uart_word == sb_word)
+    async def toggle_byte_vld(self):
+        while(True):
+            await RisingEdge(self.dut.clk)
+            self.dut.byte_in_vld.value = 1
+            await RisingEdge(self.dut.clk)
+            self.dut.byte_in_vld.value = 0
 
 
 async def cycle_rst_n(rst_n, clk):
@@ -59,22 +74,20 @@ async def cycle_rst_n(rst_n, clk):
 
  
 @cocotb.test()
-async def test_eth_rx_fsm(dut):
-    '''Test for rmii receiver'''
+async def test_test_eth_rx_fsm(dut):
+    '''Test for eth rx fsm'''
 
     tb = TB(dut)
 
-    cocotb.start_soon(cycle_rst_n(tb.dut.rx_rst_n, tb.clk))
+    cocotb.start_soon(cycle_rst_n(tb.dut.rst_n, tb.dut.clk))
 
-    await ClockCycles(tb.clk, 99)
+    cocotb.start_soon(tb.toggle_byte_vld())
 
-    message = [0x5,0x5,0x5,0x5,0x5,0xd]
+    await ClockCycles(tb.dut.clk, 100)
 
-    await tb.rmii_driver.send(message)
+    await FallingEdge(tb.dut.byte_in_vld)
+    await cocotb.start_soon(tb.send_data(PACKET))
 
-    for _ in range(NUM_WORDS):
-        await tb.rmii_driver.send()
-
-    await ClockCycles(tb.clk, 100)
+    await ClockCycles(tb.dut.clk, 100)
     dut._log.info('Test done')
 
